c680420a027854f702605a0ce65a9ba6dfa8c02b
joshwoodrum <joshuawoodrum@JoshuaWoodrumMBP.local>
2016-09-19 09:37:37 -0400

    l

---
+++ JoshWoodrum.js
@@ -0,0 +1,22 @@
+// Name, TA Name, Date
+
+// Problem 1 
+var numArray = [18, 24, 57, 98, 45 27, 3, 456, 3, 35, 85];
+
+
+
+// Problem 2 
+sum=var numArray
+
+// Problem 3
+var numArray = [1, 2, 3, 4, 5, 6, 7, 8, ,9, 10];
+
+
+// Problem 4 
+
+
+// Problem 5 
+Problem 5 Code Here
+
+// Problem 6
+Problem 6 Code Here
